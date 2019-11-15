7b2108d13191a3151f28b221c36e08a74a8d0d34
ninesintale <52729376+ninesintale@users.noreply.github.com>
2019-11-15 22:32:31 +0800

    login ajavascript

--- js/loginpage.js
+++ js/loginpage.js
@@ -4,4 +4,61 @@
  * and open the template in the editor.
  */
 
+function validateName(name){
+    var r=/[A-Za-z]{3,50}/;
+    
+    if (name === "") {
+        alert("Name can't be blank.");
+    }
+        
+    else if (r.test(name.value) === false){
+        name.setCustomValidity('Name can only contain letters, at least 3 characters long and not mroe than 50 characters');
+    }
+    else if (r.test(name.value) === true){
+        return true;
+    }
+}
 
+function validateEmail(email){
+    var r = new RegExp(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/);
+    
+    if (email === "") {
+        alert("Email can't be blank.");
+    }
+    else if (r.test(email.value) === false){
+        alert('Please enter a valid email address.');
+    }
+    else if (r.test(email.value) === true){
+        return true;
+    }
+}
+
+function validatePw(pw){
+    var r=/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,15}$/;
+    if (pw === "") {
+        alert("Password can't be blank.");
+    }
+    else if (r.test(pw.value) === false){
+        alert('Ensure password only have 1 uppercase, 1 lowercase, 1 number, 1 special character.');
+    }
+    else if (r.test(pw.value) === true){
+        return true;
+    }
+}
+
+
+
+
+
+var loginemail = document.getElementById('loginemail').value;
+    var loginpass = document.getElementById('loginpass').value;
+function validateLogin(){
+    alert("Hey");
+    loginemail.addEventListener('input', validateEmail(loginemail));
+    validateEmail(loginemail);
+    validatePw(loginpass);
+}
+document.getElementById('loginbutton').addEventListener("click", validateLogin());
+//
+//document.getElementById('regibutton').addEventListener('input', validateRegister);
+//document.getElementById('resetbutton').addEventListener('input', validateReset);
--- loginpage.php
+++ loginpage.php
@@ -45,7 +45,7 @@
                                 <!--<article class="g-recaptcha" data-sitekey="6Ld16RYTAAAAAHYoqPeGafdXADe1ya7vEuXj871m">
                                 </article>-->
                             </section>
-                            <button class="loginbutton" name="loginbutton">LOGIN</button>
+                            <button class="loginbutton" name="loginbutton" id="loginbutton">LOGIN</button>
                             <p class="separatorlink">——————— Already have an account? Sign In! ———————</p>
                         </form>
                     </article>
@@ -79,7 +79,7 @@
                                 <!--<article class="g-recaptcha" data-sitekey="6Ld16RYTAAAAAHYoqPeGafdXADe1ya7vEuXj871m">
                                 </article>-->
                             </section>
-                            <button class="regibutton" name="regibutton" id="regibutton">REGISTER</button>
+                            <button class="regibutton" name="regibutton">REGISTER</button>
                             <p class="separatorlink">—————————— New to Delta? Join us! ——————————</p>
                         </form>
                     </article>
@@ -100,7 +100,7 @@
                                 <!--<article class="g-recaptcha" data-sitekey="6Ld16RYTAAAAAHYoqPeGafdXADe1ya7vEuXj871m">
                                 </article>-->
                             </section>
-                            <button class="resetbutton" name="resetbutton" id="resetbutton">RESET PASSWORD</button>
+                            <button class="resetbutton" name="resetbutton">RESET PASSWORD</button>
                         </form>
                     </article>
                 </section>
