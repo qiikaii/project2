37a94a36f446e8f765878a129dfb2d19ee5d38b8
Kimberly <48184462+kimbderly@users.noreply.github.com>
2019-11-19 21:55:25 +0800

    products header

--- monochrome-php/header.inc.backup.php
+++ monochrome-php/header.inc.backup.php
@@ -1,17 +1,23 @@
 <?php
+
 // If theres session and last activity is 30 minutes ago, then logout
 if (isset($_SESSION['activity']) && (time() - $_SESSION['activity'] >= 1800)) {
     header("location:logout.php");
 }
 
-else if (isset($_SESSION['activity']) && (time() - $_SESSION['activity'] < 1800)) {
+// If theres session, echo out session for debugging
+if (isset($_SESSION['activity'])) {
+    echo $_SESSION['activity'];
+} else if (isset($_SESSION['activity']) && (time() - $_SESSION['activity'] < 1800)) {
     $_SESSION['activity'] = time();
 }
+
 // echo session_id();
+
 ?>
 
 <script>
-    console.log("%cSTOP", "color: red; font-size: 150px");
+    console.log("%zcSTOP", "color: red; font-size: 150px");
     console.log("%cThis is a browser feature intended for developers. ", "color= black; font-size=25px");
     console.log("%cIf someone told you to copy-paste something here to enable features or 'hack' someone's account, it is a scam and will give them access to your account.", "color= black; font-size=25px");
     console.log("%cSee https://en.wikipedia.org/wiki/Self-XSS for more information. ", "color= black; font-size=25px");
@@ -32,8 +38,8 @@ else if (isset($_SESSION['activity']) && (time() - $_SESSION['activity'] < 1800)
         </nav>
         <nav class="collapse navbar-collapse pull-right" id="navresponsive">
             <ul class="nav navbar-nav mainnavigation">
-                <li class="nav-item"><a href="../aboutus.php"><span class="glyphicon glyphicon-phone-alt"></span>ABOUT US</a></li>
-                <li class="dropdown wrapper nav-item"><a class="dropdown-toggle" data-toggle="collapse" data-target="#dropdownlist" href="#"><span class="glyphicon glyphicon-book"></span>COLLECTIONS</a>
+                <li class="nav-item"><a href="../aboutus.php">ABOUT US</a></li>
+                <li class="dropdown wrapper nav-item"><a class="dropdown-toggle" data-toggle="collapse" data-target="#dropdownlist" href="#">COLLECTIONS</a>
                     <ul class="dropdown-menu  dropdown-font" id="dropdownlist">
                         <li class="space"><a href="../celestial-php/celestial.php">CELESTIAL</a></li>
                         <li class="space"><a href="../monochrome-php/monochrome.php">MONOCHROME</a></li>
@@ -41,8 +47,7 @@ else if (isset($_SESSION['activity']) && (time() - $_SESSION['activity'] < 1800)
                 </li>
                 <?php
                 if (isset($_SESSION['acc_id'])) {
-                    echo "<li class=\"nav-item\"><a href=\"../cartpage.php\"><span class=\"glyphicon glyphicon-shopping-cart\"></span>CART</a></li>       
-                <li class=\"nav-item\"><a href=\"../account.php\"><span class=\"glyphicon glyphicon-user\"></span> ACCOUNT</a></li>
+                    echo "<li class=\"nav-item\"><a href=\"../cartpage.php\">CART</a></li>                    
                 <li class=\"nav-item\"><a href=\"../logout.php\"><span class=\"glyphicon glyphicon-log-out\"></span> LOGOUT</a></li>";
                 } else {
                     echo "<li class=\"nav-item\"><a href=\"../loginpage.php\"><span class=\"glyphicon glyphicon-log-in\"></span> LOGIN</a></li>";
--- monochrome-php/header.inc.php
+++ monochrome-php/header.inc.php
@@ -1,23 +1,15 @@
 <?php
-
 // If theres session and last activity is 30 minutes ago, then logout
 if (isset($_SESSION['activity']) && (time() - $_SESSION['activity'] >= 1800)) {
     header("location:logout.php");
-}
-
-// If theres session, echo out session for debugging
-if (isset($_SESSION['activity'])) {
-    echo $_SESSION['activity'];
 } else if (isset($_SESSION['activity']) && (time() - $_SESSION['activity'] < 1800)) {
     $_SESSION['activity'] = time();
 }
-
 // echo session_id();
-
 ?>
 
 <script>
-    console.log("%zcSTOP", "color: red; font-size: 150px");
+    console.log("%cSTOP", "color: red; font-size: 150px");
     console.log("%cThis is a browser feature intended for developers. ", "color= black; font-size=25px");
     console.log("%cIf someone told you to copy-paste something here to enable features or 'hack' someone's account, it is a scam and will give them access to your account.", "color= black; font-size=25px");
     console.log("%cSee https://en.wikipedia.org/wiki/Self-XSS for more information. ", "color= black; font-size=25px");
@@ -38,8 +30,8 @@ if (isset($_SESSION['activity'])) {
         </nav>
         <nav class="collapse navbar-collapse pull-right" id="navresponsive">
             <ul class="nav navbar-nav mainnavigation">
-                <li class="nav-item"><a href="../aboutus.php">ABOUT US</a></li>
-                <li class="dropdown wrapper nav-item"><a class="dropdown-toggle" data-toggle="collapse" data-target="#dropdownlist" href="#">COLLECTIONS</a>
+                <li class="nav-item"><a href="../aboutus.php"><span class="glyphicon glyphicon-phone-alt"></span>ABOUT US</a></li>
+                <li class="dropdown wrapper nav-item"><a class="dropdown-toggle" data-toggle="collapse" data-target="#dropdownlist" href="#"><span class="glyphicon glyphicon-book"></span>COLLECTIONS</a>
                     <ul class="dropdown-menu  dropdown-font" id="dropdownlist">
                         <li class="space"><a href="../celestial-php/celestial.php">CELESTIAL</a></li>
                         <li class="space"><a href="../monochrome-php/monochrome.php">MONOCHROME</a></li>
@@ -47,7 +39,8 @@ if (isset($_SESSION['activity'])) {
                 </li>
                 <?php
                 if (isset($_SESSION['acc_id'])) {
-                    echo "<li class=\"nav-item\"><a href=\"../cartpage.php\">CART</a></li>                    
+                    echo "<li class=\"nav-item\"><a href=\"../cartpage.php\"><span class=\"glyphicon glyphicon-shopping-cart\"></span>CART</a></li>       
+                <li class=\"nav-item\"><a href=\"../account.php\"><span class=\"glyphicon glyphicon-user\"></span> ACCOUNT</a></li>
                 <li class=\"nav-item\"><a href=\"../logout.php\"><span class=\"glyphicon glyphicon-log-out\"></span> LOGOUT</a></li>";
                 } else {
                     echo "<li class=\"nav-item\"><a href=\"../loginpage.php\"><span class=\"glyphicon glyphicon-log-in\"></span> LOGIN</a></li>";
