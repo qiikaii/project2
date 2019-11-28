76bfd6f0ed98a3425f655745b5ad452ace984331
QIIKAII <sharpnshiny22@gmail.com>
2019-11-29 00:59:42 +0800

    ADEQWEQ

--- monochrome-php/product_template.php
+++ monochrome-php/product_template.php
@@ -1,5 +1,7 @@
 <?php
-
+ini_set('session.use_only_cookies', 1);
+ini_set('session.cookie_secure', 1);
+ini_set('session.cookie_httponly', 1);
 
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
